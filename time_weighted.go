d25488750a8727ce8afafb8409a9648b43bc1ecd
Sam Watson <watson.sam95@gmail.com>
2022-04-20 21:05:21 +0200

    div by 0

--- time_weighted.go
+++ time_weighted.go
@@ -6,6 +6,9 @@ func (ro *RollingTimeObject) WeightValues(date time.Time) {
 	weighted := make(valueAndTimes, len(ro.values))
 	for _, vad := range ro.values {
 		days := date.Sub(vad.date).Hours() / 24
+		if days == 0 {
+			days = 1
+		}
 		vad.weighted = vad.value * (1 / (ro.weight * days))
 		weighted = append(weighted, vad)
 	}
